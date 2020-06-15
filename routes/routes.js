1e6dc5a920ddcfd76b27934d1f13a14d6abf160c
sachit_katri <sachitkumarkatri@gmail.com>
2020-06-15 18:30:24 +0530

    Console key removed

--- routes/routes.js
+++ routes/routes.js
@@ -5,12 +5,12 @@ const NewsAPI = require('newsapi');
 const { response } = require('express');
 const newsapi = new NewsAPI('process.env.API_KEY');
 const api_key = process.env.API_KEY
-console.log(api_key)
+
 
 router.get('/', async (req,res) => {
 
     const api_url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey='+api_key
-    console.log(api_url)
+
     const fetch_response = await fetch(api_url)
 
     const json = await fetch_response.json()

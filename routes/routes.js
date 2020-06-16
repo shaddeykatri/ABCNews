bf17b7c37ae406ad6818a73456b66c0522eff8dd
sachit_katri <sachitkumarkatri@gmail.com>
2020-06-16 14:20:25 +0530

    Request replaces fetch

--- routes/routes.js
+++ routes/routes.js
@@ -9,17 +9,26 @@ const api_key = process.env.API_KEY
 
 router.get('/', async (req,res) => {
 
+    // const api_url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey='+api_key
+
+    // const fetch_response = await fetch(api_url)
+
+    // const json = await fetch_response.json()
+    // res.json(json)
+
     var request = require('request');
-var options = {
-  'method': 'GET',
-  'url': 'https://news-hexaware.herokuapp.com/news',
-  'headers': {
-  }
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
+    var options = {
+     'method': 'GET',
+     'url': 'https://newsapi.org/v2/top-headlines?country=in&apiKey='+api_key,
+    'headers': {
+    }
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        console.log(response.body);
+        res.json(response.body);
+        });
+
 })
 
 module.exports = router
